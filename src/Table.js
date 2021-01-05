7af01c27f8b777923c75412d6fc84eb26202ac5d
Vasilika Hasanoska <vasilika.h@hotmail.com>
2021-01-05 11:03:30 +0100

    Initial Commit

---
+++ src/Table.js
@@ -0,0 +1,90 @@
+/*class component*/
+/*import React, {Component} from 'react'
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+          </tr>
+          <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+          </tr>
+          <tr>
+            <td>Dee</td>
+            <td>Aspiring actress</td>
+          </tr>
+          <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default Table*/
+
+/*simple component*/
+import React, {Component} from 'react'
+const TableHeader = ()=> {
+	return(
+	<thead>
+	<tr>
+	<th>Name</th>
+	<th>Job</th>
+	</tr>
+	</thead>
+	)
+}
+
+const TableBody =(props) => {
+	const rows=props.characterData.map((row,index)=>{
+		return(
+		<tr key={index}>
+		<td>{row.name}</td>
+		<td>{row.job}</td>
+		<td>
+		<button onClick={()=>props.removeCharacter(index)}>Delete</button>
+		</td>
+		</tr>
+		)
+	})
+	return <tbody>{rows}</tbody>
+	
+}
+
+const Table=(props)=>{
+	const{characterData,removeCharacter}=props
+	return(
+	<table>
+	<TableHeader/>
+	<TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+	</table>
+	)
+}
+
+// class Table extends Component {
+  // render() {
+    // const {characterData} = this.props
+
+    // return (
+      // <table>
+        // <TableHeader />
+        // <TableBody characterData={characterData} />
+      // </table>
+    // )
+  // }
+// }
+export default Table
